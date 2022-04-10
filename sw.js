6c6f82f0ab0d6c22ae1cfa3bbdd796a448c8246d
Shakil Alam <itxshakil@gmail.com>
2022-04-10 13:31:15 +0530

    delete old cache and return empty response on fetch error

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-var cacheName = 'todoApp-v1.4.2';
-// Change main js file name
-var filesToCache = [
+const cacheName = 'todoApp-v1.6.0';
+
+const filesToCache = [
     '/',
     '/index.html',
     'index.html?source=pwa',
@@ -11,7 +11,7 @@ var filesToCache = [
     '/js/classes/Storage.js',
     '/js/classes/Task.js',
     '/js/classes/TaskManager.js',
-    'app.webmanifest',
+    '/app.webmanifest',
     '/about.html',
     '/faq.html',
     '/share.html',
@@ -19,20 +19,40 @@ var filesToCache = [
 ];
 
 // Start the service worker and cache all of the app's content
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(filesToCache)));
 });
 
-// Serve Cache content when offline
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key !== cacheName) {
+                return caches.delete(key);
+            }
+        }));
+    }));
+});
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => {
+        if (response) {
+            return response;
+        }
+
+        return fetch(e.request).then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+            }
+
+            const responseToCache = response.clone();
+            caches.open(cacheName).then((cache) => {
+                cache.put(e.request, responseToCache);
+            });
+
+            return response;
+        }).catch((e) => {
+            console.error(e);
+            return new Response();
+        });
+    }));
 });
